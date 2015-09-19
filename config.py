a9631e4a1c9652a3fbb1404f9f5636a6a4082203
ExtremelySeriousChicken <wijaya@umich.edu>
2015-09-19 11:08:02 -0400

    entire skeleton of the project

---
+++ config.py
@@ -0,0 +1,35 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = 'IMAGETRAVELLERDABEST'
+    # we dont use SQLALCHEMY BUT IF WE DO
+    # SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    # WE DONT USE MAIL AS WELL
+    # FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    # FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    # FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    # SKIP ALL THE MAILS AND DATABASE STUFF
+
+class TestingConfig(config):
+    TESTING = True
+    # Skipping the database config
+
+class ProductionConfig(Config):
+    # SKIPPING THE Database again
+
+config = {
+    'development' : DevelopmentConfig,
+    'testing'     : TestingConfig,
+    'production'  : ProductionConfig,
+
+    'default'     : DevelopmentConfig
+}
