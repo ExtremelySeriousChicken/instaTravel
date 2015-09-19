fe84595ce43f00fe8f8ba9da6db542e26d3b0f5a
Ritvik Upadhyaya <ritvikupadhyaya@gmail.com>
2015-09-19 16:58:09 -0400

    Amadeus and quotation bug fix

--- instagramAPI.py
+++ instagramAPI.py
@@ -1,5 +1,6 @@
 from instagram.client import InstagramAPI
-import json
+import amadeus
+from amadeus import Flights
 
 
 access_token = "569470452.360cc24.08adf989f9f147afa7d72c181d53b7a9"
@@ -7,8 +8,9 @@ client_id = "360cc24cb8534f7cbbfb06d7ee1e26a3"
 client_secret = "4e1026ba847841eab8797101ceb9680a"
 LOCATIONS = []
 
-api = InstagramAPI(access_token="access_token", client_secret="client_secret")
-
+api = InstagramAPI(access_token=access_token, client_secret=client_secret)
+amadeus_api_key = 'RzGfJPsCmYIRlDmTi6Og2BAYAVviEAdh'
+flights = Flights(amadeus_api_key)
 
 def getUserLikedInformation():
     userMediaValue, next_ = api.user_liked_media()
@@ -28,7 +30,8 @@ def printUserData():
     for loc in LOCATIONS:
         print (loc[0], loc[1])
 
-
+# def AirportInformation(lat, long):
+	
 
 getUserLikedInformation()
 printUserData()
